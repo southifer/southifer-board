21e86384d2869a12736fb05efa3a022d8d637e88
Noire <kyuzako@gmail.com>
2024-11-23 18:02:44 +0800

    --

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -28,7 +28,7 @@ const NavigationBar = ({userInformation, isUserAdmin}) => {
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6"
+                className={`w-6 h-6 transform transition-transform ${isNavOpen ? 'rotate-180' : ''}`}
             >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
